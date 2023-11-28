4b4a2ab75577eb5f5afefbc994db930609ca7476
Gabriel Hug <me@Gabriels-Air.fritz.box>
2023-11-28 12:08:29 +0100

    merge with origin

--- fluid_simulation.py
+++ fluid_simulation.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import taichi as ti
 import taichi.math as tm
 from taichi.examples.patterns import taichi_logo
+#fountain class
+from fountain import Fountain
 
 # configuration and hyper parameters
 window_height = 512
@@ -13,6 +15,11 @@ rho = 1.0
 g = ti.Vector([0, -9.81])
 color_decay = 0.001 # set 0 for no color decay/fading
 color_multiplier = 1 - color_decay
+
+
+#shift of the fountain 
+fountain_offset = 0
+
 
 # init step for taichi lang
 ti.init(arch=ti.cpu)
@@ -115,9 +122,22 @@ def update_advection_step():
     
         #ADDED: set velocity at boundary condition
         if bc_mask[i, j] == 1:
-            velocity_field_u[i, j] = ti.Vector([0, 0])
-            continue
-                
+
+            velocity_field_u[i, j] = ti.Vector([0, 0])#ti.Vector([0, -9.81])#*stepsize_delta_t
+            # if bc_mask[i - 1, j] == 0 and bc_mask[i, j - 1] == 1 and bc_mask[i, j + 1] == 1:
+            #     velocity_field_u[i + 1, j] = -velocity_field_u[i - 1, j]
+            # elif bc_mask[i + 1, j] == 0 and bc_mask[i, j - 1] == 1 and bc_mask[i, j + 1] == 1:
+            #     velocity_field_u[i - 1, j] = -velocity_field_u[i + 1, j]
+            # elif bc_mask[i, j - 1] == 0 and bc_mask[i - 1, j] == 1 and bc_mask[i + 1, j] == 1:
+            #     velocity_field_u[i, j + 1] = -velocity_field_u[i, j - 1]
+            # elif bc_mask[i, j + 1] == 0 and bc_mask[i - 1, j] == 1 and bc_mask[i + 1, j] == 1:
+            #     velocity_field_u[i, j - 1] = -velocity_field_u[i, j + 1]  
+
+            #falling ball, when collides with ground it explodes lol 
+            #bc_mask[i, j] = 0
+            #bc_mask[i, int(j - 9.81*stepsize_delta_t)] = 1
+
+
         # 1. Dertime velocity u_ij at grid point
         velocity_u_ij = velocity_field_u[i, j]
         # 2. Integrate position for a timestep of - delta_t
@@ -128,7 +148,18 @@ def update_advection_step():
         position_source = position_now - velocity_u_ij * stepsize_delta_t
         # 3. Interpolate q at x_source to obtain q_source
         # q seems to be the color value
-        color_q_source = bilerp(color_field_q, position_source) #this interpolation creates problem in aroiund the ball as the color is interpolated from the sphere too
+
+        
+        #this interpolation creates problem in aroiund the ball as the color is interpolated from the sphere too
+        color_q_source = bilerp(color_field_q, position_source)
+
+        # if bc_mask[i, j] == 1:
+        #     bc_mask[i, j] = 0
+        #     new_x = int(position_source[0])
+        #     new_y = int(position_source[1])
+        #     bc_mask[new_x, new_y] = 1
+        #     color_q_source = ti.Vector([0, 255, 255])
+        
         # 4. Assign q_ij = q_source for next time step
         new_color_field_q[i, j] = color_q_source * color_multiplier
         # redo step 3 for velocity as well. Actually q is the color as well as the velocity
@@ -154,19 +185,21 @@ def update_advection_step():
 
 
 @ti.kernel
-def update_externalforces_step():
+def update_externalforces_step(offset : ti.template()):
     # I think the only external force is gravity.
     # according to ETH lecture 4 slide 36,
     # we can use a simple forward Euler integration
     for i, j in velocity_field_u:
         velocity_field_u[i, j] = velocity_field_u[i, j] + stepsize_delta_t*g/rho
     # apparently, only do it for u and not q -> ignore the color field here
-
+    
     # artificial force for the input
-    fountain_pixel_width = 11 // 2
+    fountain_pixel_width = 11 // 2  + 10
+
+    # seems like source only moves when offset becomes lerger than fountain width
     for k in range(-fountain_pixel_width, fountain_pixel_width+1):
-        velocity_field_u[int(window_width/2)-k, 0] = ti.Vector([0, 700.0])
-        color_field_q[int(window_width/2)-k, 0] = 1
+        velocity_field_u[int(window_width/2 + offset)-k, 0] = ti.Vector([0, 900.0])
+        color_field_q[int(window_width/2 + offset)-k, 0] = 1
 
     # apply boundary conditions
     # TODO: apply boundary conditions
@@ -233,29 +266,31 @@ def update_pressure_bc(pressure_field_p: ti.template(), bc_mask: ti.template()):
     # update the pressure at boundary conditions
     for i, j in pressure_field_p:
         if bc_mask[i, j] == 1:
-            if bc_mask[i - 1, j] == 0 and bc_mask[i, j - 1] == 1 and bc_mask[i, j + 1] == 1:
-                pressure_field_p[i, j] = pressure_field_p[i - 1, j]
-            elif bc_mask[i + 1, j] == 0 and bc_mask[i, j - 1] == 1 and bc_mask[i, j + 1] == 1:
-                pressure_field_p[i, j] = pressure_field_p[i + 1, j]
-            elif bc_mask[i, j - 1] == 0 and bc_mask[i - 1, j] == 1 and bc_mask[i + 1, j] == 1:
-                pressure_field_p[i, j] = pressure_field_p[i, j - 1]
-            elif bc_mask[i, j + 1] == 0 and bc_mask[i - 1, j] == 1 and bc_mask[i + 1, j] == 1:
-                pressure_field_p[i, j] = pressure_field_p[i, j + 1]
-            elif bc_mask[i - 1, j] == 0 and bc_mask[i, j + 1] == 0:
-                pressure_field_p[i, j] = (pressure_field_p[i - 1, j] + pressure_field_p[i, j + 1]) / 2.0
-            elif bc_mask[i + 1, j] == 0 and bc_mask[i, j + 1] == 0:
-                pressure_field_p[i, j] = (pressure_field_p[i + 1, j] + pressure_field_p[i, j + 1]) / 2.0
-            elif bc_mask[i - 1, j] == 0 and bc_mask[i, j - 1] == 0:
-                pressure_field_p[i, j] = (pressure_field_p[i - 1, j] + pressure_field_p[i, j - 1]) / 2.0
-            elif bc_mask[i + 1, j] == 0 and bc_mask[i, j - 1] == 0:
-                pressure_field_p[i, j] = (pressure_field_p[i + 1, j] + pressure_field_p[i, j - 1]) / 2.0
+            pressure_field_p[i, j] = 0
+        # if bc_mask[i, j] == 1:
+        #     if bc_mask[i - 1, j] == 0 and bc_mask[i, j - 1] == 1 and bc_mask[i, j + 1] == 1:
+        #         pressure_field_p[i, j] = pressure_field_p[i - 1, j]
+        #     elif bc_mask[i + 1, j] == 0 and bc_mask[i, j - 1] == 1 and bc_mask[i, j + 1] == 1:
+        #         pressure_field_p[i, j] = pressure_field_p[i + 1, j]
+        #     elif bc_mask[i, j - 1] == 0 and bc_mask[i - 1, j] == 1 and bc_mask[i + 1, j] == 1:
+        #         pressure_field_p[i, j] = pressure_field_p[i, j - 1]
+        #     elif bc_mask[i, j + 1] == 0 and bc_mask[i - 1, j] == 1 and bc_mask[i + 1, j] == 1:
+        #         pressure_field_p[i, j] = pressure_field_p[i, j + 1]
+        #     elif bc_mask[i - 1, j] == 0 and bc_mask[i, j + 1] == 0:
+        #         pressure_field_p[i, j] = (pressure_field_p[i - 1, j] + pressure_field_p[i, j + 1]) / 2.0
+        #     elif bc_mask[i + 1, j] == 0 and bc_mask[i, j + 1] == 0:
+        #         pressure_field_p[i, j] = (pressure_field_p[i + 1, j] + pressure_field_p[i, j + 1]) / 2.0
+        #     elif bc_mask[i - 1, j] == 0 and bc_mask[i, j - 1] == 0:
+        #         pressure_field_p[i, j] = (pressure_field_p[i - 1, j] + pressure_field_p[i, j - 1]) / 2.0
+        #     elif bc_mask[i + 1, j] == 0 and bc_mask[i, j - 1] == 0:
+        #         pressure_field_p[i, j] = (pressure_field_p[i + 1, j] + pressure_field_p[i, j - 1]) / 2.0
 
 @ti.kernel
 def set_inverted_colorfield():
     for I in ti.grouped(color_field_q):
         display_inverted_color_field[I] = 1-color_field_q[I]
 
-def update_all_steps():
+def update_all_steps(fountain : ti.template()):
     # Split the Navier-Stokes equation into four parts. See ETH lecture 4 slide 30
     # Treat each term seperately, in the following order
     # 1) Advection
@@ -267,7 +302,8 @@ def update_all_steps():
     update_advection_step()
     # ignore viscosity. note from lecture:
     # "numerical dissipation due to Semi-Lagrangian advection is often sufficient"
-    update_externalforces_step()
+    #print("offset: ", fountain.offset)
+    update_externalforces_step(fountain.offset)
     update_pressure_step()
 
     set_inverted_colorfield()
@@ -293,17 +329,21 @@ def init_fields():
 
 
 def main():
-
+    
     window = ti.ui.Window("Simulation", (window_width, window_height), vsync=False)
     canvas = window.get_canvas()
 
     init_fields()
 
     paused = False
+    #create fountain object
+    fountain = Fountain(11 // 2)
+
     while window.running:
+        
         if not paused:
             # update step
-            update_all_steps()
+            update_all_steps(fountain)
 
         if window.get_event(ti.ui.PRESS):
             e = window.event
@@ -311,6 +351,12 @@ def main():
                 break
             elif e.key == "p":
                 paused = not paused
+            elif e.key == "d":
+                fountain.offset += 8
+                #print(fountain.offset)
+            elif e.key == "a":
+                fountain.offset -= 8    
+
 
         canvas.set_image(display_inverted_color_field)
         window.show()
